5931e16a2b025b145188760211ffc87b21179f19
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-09-20 17:56:26 +0200

    New form setup

--- server/server.js
+++ server/server.js
@@ -12,18 +12,10 @@ app.use(require("./routes/record"));
 const dbo = require("./db/conn");
 
 app.listen(port, () => {
-  
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
 
   });
   console.log(`Server is running on port: ${port}`);
-});
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
